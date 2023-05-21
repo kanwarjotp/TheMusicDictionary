d712796491211420802cdb614b91522350f398c9
Kanwar Jot Parkash Singh <52690070+kanwarjotp@users.noreply.github.com>
2023-05-21 09:25:56 +0530

    refactored javascript files conatining recording logic etc.

---
+++ static/scripts/profile.js
@@ -0,0 +1,91 @@
+// record song function
+async function getMedia(constraints) {
+    let stream = null;
+  
+    try {
+        stream = await navigator.mediaDevices.getUserMedia({video: false, audio:true});
+        first_track = stream.getAudioTracks()[0]
+        // console.log(first_track)
+      
+        //   create a media recorder
+        recorder = new MediaRecorder(stream)
+        recorder.addEventListener('dataavailable', handleRecData)
+
+        recorder.start()
+        // alert(recorder.state)
+        updtButton(1)
+
+        setTimeout(function(){
+            recorder.stop()
+            first_track.stop()
+            // alert("recording stopped")
+            updtButton(2)
+        }
+        , 10000)
+
+    } catch (err) {
+        /* handle the error */
+        alert("mic denied!")
+        alert(err)
+    }
+  }
+
+//   using AJAX to send the BLOB data to the backend.
+function handleRecData(event){
+    console.log(event.data)
+    const rec_sample = new Blob([event.data], {'type': 'audio/wav'})
+    const sample_url = URL.createObjectURL(rec_sample)
+    console.log(sample_url)
+    const rec_audio = new Audio(sample_url)
+
+    // sending the data to server 
+    const send_req = new XMLHttpRequest()
+    var save_form = new FormData()
+    save_form.append("sample_data", rec_sample, "rec_sample")
+    send_req.open("POST", "/process_sample", true)
+    send_req.send(save_form)
+
+    // request song identification
+    askForPrediction()
+}
+
+
+async function askForPrediction(){
+    // grabbing URL from the HTML document
+    const pred_url = document.getElementById("prediction-link").innerHTML
+
+    fetch(pred_url)
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data)
+        const pred_div = document.getElementById("prediction")
+        pred_div.innerHTML = data["ans"]
+        updtButton(3)
+
+        // updating the button to record a new song
+        setTimeout(function(){updtButton(0)}, 5000)
+
+    })
+}
+
+
+function updtButton(value) {
+    const bttn = document.getElementById("rec-bttn")
+    if (value == 0) {
+        bttn.disabled = false
+        bttn.innerHTML = "click to record"
+        bttn.style = "background-color: #0d6efd"
+    } else if (value == 1) {
+        bttn.disabled = true
+        bttn.innerHTML = "recording"
+        bttn.style = " background-color: red;"
+    } else if (value == 2) {
+        bttn.disabled = true
+        bttn.innerHTML = "working"
+        bttn.style = " background-color: black;"
+    } else if (value == 3) {
+        bttn.disabled = true
+        bttn.innerHTML = "song found!"
+        bttn.style = " background-color: green;"
+    } 
+}
